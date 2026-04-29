b6e87106f9ec68f1ef270278b448312f08405e17
DyshaDyshez <70228682+DyshaDyshez@users.noreply.github.com>
2026-04-30 00:45:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    SUPABASE_URL: "https://yyohojhvayfcwiqrdiqf.supabase.co",
+    SUPABASE_URL: 'https://supabase-proxy-nso0.onrender.com',
     SUPABASE_KEY: "sb_publishable_DEAK6DWGz8_swJaVLKdrVA_h1c5_Glx"
 };
