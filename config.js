4c24161c658121461c7400916cf73609409932d8
DyshaDyshez <70228682+DyshaDyshez@users.noreply.github.com>
2026-04-30 00:59:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    SUPABASE_URL: 'https://corsproxy.io/?' + encodeURIComponent('https://yyohojhvayfcwiqrdiqf.supabase.co')
+    SUPABASE_URL: "https://yyohojhvayfcwiqrdiqf.supabase.co",
     SUPABASE_KEY: "sb_publishable_DEAK6DWGz8_swJaVLKdrVA_h1c5_Glx"
 };
